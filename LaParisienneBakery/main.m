7f9d1f874b12dd6faca95c8ccb4baaeb5ad4e0cb
cynthia besada <cbesa002@fiu.edu>
2013-05-04 17:12:42 -0400

    af

---
+++ LaParisienneBakery/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LaParisienneBakery
+//
+//  Created by cynthia besada on 5/4/13.
+//  Copyright (c) 2013 cynthia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
